81c9474c69dc24206e88b22fe457766a4e1709ed
TNSHR <rohandw0@gmail.com>
2024-05-25 07:23:34 +0530

    Check

--- country-search/src/CountriesSearch.jsx
+++ country-search/src/CountriesSearch.jsx
@@ -20,16 +20,15 @@ const filteredCountries=countries.filter(country=>country.name.common.toLowerCas
             placeholder='Search for a country..'
             value={searchItem}
             onChange={e=>setSearchItem(e.target.value)} style={{textAlign:'center', height:"30px",width:"500px"}}/>
+
             <div style={{display:'flex',flexWrap:'wrap',gap:'20px',justifyContent:'center'}}>
-                {filteredCountries.map(country=>(
-                    <div key={country.cca3} className='countryCard' style={{display:'flex',flexDirection:'column',width:'100px',borderRadius:'8px',padding:'10px'}}>
+                {filteredCountries.length>0?(filteredCountries.map(country=>(<div key={country.cca3} className='countryCard' style={{display:'flex',flexDirection:'column',width:'100px',borderRadius:'8px',padding:'10px'}}>
                         <img src={country.flags.png} alt={'Flag of ${country.name.common}'}  className='countryCard' style={{width:'100px',height:'auto',marginBottom:'10px'}}/>
-                        <span style={{margin:'0',fontSize:'10px',textAlign:'center'}} className='countryCard'>{country.name.common}</span>
-                    </div>
-                ))}
+                        <p style={{margin:'0',fontSize:'10px',textAlign:'center'}} className='countryCard'>{country.name.common}</p>
+                    </div>))):(<p>No matching countries found</p>)}
             </div>
         </div>
-    )
+    );
 }
  export default CountriesSearch;
 
