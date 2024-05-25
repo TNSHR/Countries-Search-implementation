63e74613ddd6c7c400b19195fd4f0c43c73fff78
TNSHR <rohandw0@gmail.com>
2024-05-25 07:11:31 +0530

    Second change

--- country-search/src/CountriesSearch.jsx
+++ country-search/src/CountriesSearch.jsx
@@ -13,17 +13,18 @@ const filteredCountries=countries.filter(country=>country.name.common.toLowerCas
 
 
     return(
-        <div style={{margin:'0',padding:'20px',display:'flex',flexDirection:'column',alignItems:'center'}}>
+        <div className='countryCard' style={{margin:'0',padding:'20px',display:'flex',flexDirection:'column',alignItems:'center'}}>
             <input
+            className='countryCard'
             type='text'
             placeholder='Search for a country..'
             value={searchItem}
             onChange={e=>setSearchItem(e.target.value)} style={{textAlign:'center', height:"30px",width:"500px"}}/>
             <div style={{display:'flex',flexWrap:'wrap',gap:'20px',justifyContent:'center'}}>
                 {filteredCountries.map(country=>(
-                    <div key={country.cca3} style={{display:'flex',flexDirection:'column',width:'100px',borderRadius:'8px',padding:'10px'}}>
-                        <img src={country.flags.png} alt={'Flag of ${country.name.common}'} style={{width:'100px',height:'auto',marginBottom:'10px'}}/>
-                        <span style={{margin:'0',fontSize:'10px',textAlign:'center'}}>{country.name.common}</span>
+                    <div key={country.cca3} className='countryCard' style={{display:'flex',flexDirection:'column',width:'100px',borderRadius:'8px',padding:'10px'}}>
+                        <img src={country.flags.png} alt={'Flag of ${country.name.common}'}  className='countryCard' style={{width:'100px',height:'auto',marginBottom:'10px'}}/>
+                        <span style={{margin:'0',fontSize:'10px',textAlign:'center'}} className='countryCard'>{country.name.common}</span>
                     </div>
                 ))}
             </div>
