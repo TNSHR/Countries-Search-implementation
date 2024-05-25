f181c6d7c1324df23e004b357099c9672085867d
TNSHR <rohandw0@gmail.com>
2024-05-25 07:47:00 +0530

    some changes

--- country-search/src/CountriesSearch.jsx
+++ country-search/src/CountriesSearch.jsx
@@ -13,19 +13,19 @@ const filteredCountries=countries.filter(country=>country.name.common.toLowerCas
 
 
     return(
-        <div className='countryCard' style={{margin:'0',padding:'20px',display:'flex',flexDirection:'column',alignItems:'center'}}>
+        <div className='countryCard' >
             <input
             className='countryCard'
             type='text'
             placeholder='Search for a country..'
             value={searchItem}
-            onChange={e=>setSearchItem(e.target.value)} style={{textAlign:'center', height:"30px",width:"500px"}}/>
+            onChange={e=>setSearchItem(e.target.value)} />
 
-            <div style={{display:'flex',flexWrap:'wrap',gap:'20px',justifyContent:'center'}}>
+            <div >
                 {filteredCountries.length>0?(filteredCountries.map(country=>(<div key={country.cca3} className='countryCard' style={{display:'flex',flexDirection:'column',width:'100px',borderRadius:'8px',padding:'10px'}}>
-                        <div style={{container:'inherit'}}><img src={country.flags.png} alt={'Flag of ${country.name.common} '}  className='countryCard' style={{width:'100px',height:'auto',marginBottom:'10px'}}/>
-                        <p style={{margin:'0',fontSize:'10px',textAlign:'center'}} className='countryCard'>{country.name.common}</p></div>
-                    </div>))):(<p>No matching countries found</p>)}
+                        <img src={country.flags.png} alt={'Flag of ${country.name.common} '}  className='countryCard' style={{width:'100px',height:'auto',marginBottom:'10px'}}/>
+                        <p  className='countryCard'>{country.name.common}</p></div>
+                    ))):(<p>No matching countries found</p>)}
             </div>
         </div>
     );
