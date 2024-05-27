9385a58cd4f34532444bdd8060369ce865bd9a65
TNSHR <rohandw0@gmail.com>
2024-05-27 12:35:30 +0530

    udated commit

--- country-search/src/CountriesSearch.jsx
+++ country-search/src/CountriesSearch.jsx
@@ -1,58 +1,50 @@
 import React, { useState, useEffect } from 'react';
-import CountryCard from './countriesCard';
+
 import './countries.css'
 
-const CountriesSearch = () => {
-  const [countries, setCountries] = useState([]);
-  const [searchTerm, setSearchTerm] = useState('');
-  const [filteredCountries, setFilteredCountries] = useState([]);
 
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await fetch('https://restcountries.com/v3.1/all');
-        const data = await response.json();
-        setCountries(data);
-      } catch (error) {
-        console.error('Error fetching data:', error);
-      }
-    };
+function CountriesSearch(){
 
-    fetchData();
-  }, []);
+const[countries,setCountries]=useState([]);
+const[searchItem, setSearchItem]=useState('');
 
-  const handleSearch = (event) => {
-    setSearchTerm(event.target.value.toLowerCase());
-  };
-
-  useEffect(() => {
-    const filtered = countries.filter((country) =>
-      country.name.common.toLowerCase().includes(searchTerm)
-    );
-    setFilteredCountries(filtered);
-  }, [countries, searchTerm]);
-
-  const displayedCountries = searchTerm ? filteredCountries : countries;
-
-  return (
-    <div>
-      <input
-      className='searchbar'
-        type="text"
-        placeholder="Search Countries"
-        value={searchTerm}
-        onChange={handleSearch}
-      />
-      <div className="countryCard">
-        {displayedCountries.map((country) => (
-          <CountryCard key={country.cca3} flag={country.flags.png} name={country.name.common} />
-        ))}
-      </div>
-    </div>
-  );
-};
-
-export default CountriesSearch;
+useEffect(()=>{
+    fetch(' https://restcountries.com/v3.1/all')
+    .then(response=>response.json())
+    .then(data=>setCountries(data))
+    .catch(error=>console.
+      error('Error fetching data',error))
+},[]);
+const filteredCountries=countries
+.filter(country=>country.name.common
+  .toLowerCase()
+  .includes(searchItem
+    .toLowerCase()))
+;
 
 
+    return(
+        <div style={{margin:'0',
+        padding:'20px',
+        display:'flex',flexDirection:'column',
+        alignItems:'center'}}>
+            <input
+            type='text'
+            placeholder='Search for a country..'
+            value={searchItem}
+            onChange={e=>setSearchItem(e.target.value)} style={{textAlign:'center', height:"30px",width:"500px"}}/>
+            <div style={{display:'flex',flexWrap:'wrap',gap:'20px',justifyContent:'center'}}>
+                {filteredCountries.map(country=>(
+                    <div key={country.cca3} style={{display:'flex',flexDirection:'column',width:'100px',borderRadius:'8px',padding:'10px'}}>
+                        <img src={country.flags.png} alt={'Flag of ${country.name.common}'} style={{width:'100px',height:'auto',marginBottom:'10px'}}/>
+                        <span style={{margin:'0',fontSize:'10px',textAlign:'center'}}>{country.name.common}</span>
+                    </div>
+                ))}
+            </div>
+        </div>
+    )
+}
+ export default CountriesSearch;
+
+  
   
