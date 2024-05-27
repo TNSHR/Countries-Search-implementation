1b6ce35602a22d529c082d093ce4db7bfd2f0b79
TNSHR <rohandw0@gmail.com>
2024-05-27 13:06:17 +0530

    m1

--- country-search/src/CountriesSearch.jsx
+++ country-search/src/CountriesSearch.jsx
@@ -27,15 +27,15 @@ const filteredCountries=countries
         <div style={{margin:'0',
         padding:'20px',
         display:'flex',flexDirection:'column',
-        alignItems:'center'}}>
+        alignItems:'center'}} className="countryCard">
             <input
             type='text'
             placeholder='Search for a country..'
             value={searchItem}
             onChange={e=>setSearchItem(e.target.value)} style={{textAlign:'center', height:"30px",width:"500px"}}/>
-            <div style={{display:'flex',flexWrap:'wrap',gap:'20px',justifyContent:'center'}}>
+            <div  className="countryCard" style={{display:'flex',flexWrap:'wrap',gap:'20px',justifyContent:'center'}}>
                 {filteredCountries.map(country=>(
-                    <div key={country.cca3} style={{display:'flex',flexDirection:'column',width:'100px',borderRadius:'8px',padding:'10px'}}>
+                    <div className="countryCard" key={country.cca3} style={{display:'flex',flexDirection:'column',width:'100px',borderRadius:'8px',padding:'10px'}}>
                         <img src={country.flags.png} alt={'Flag of ${country.name.common}'} style={{width:'100px',height:'auto',marginBottom:'10px'}}/>
                         <span style={{margin:'0',fontSize:'10px',textAlign:'center'}}>{country.name.common}</span>
                     </div>
