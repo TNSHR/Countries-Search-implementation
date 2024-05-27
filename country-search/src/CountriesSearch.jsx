7d2ff799c1beb4617ab847d931145ca179f41d5f
TNSHR <rohandw0@gmail.com>
2024-05-27 09:10:20 +0530

    New changes

--- country-search/src/CountriesSearch.jsx
+++ country-search/src/CountriesSearch.jsx
@@ -1,4 +1,5 @@
 import React, {useState,useEffect} from 'react';
+import './countries.css'
 
 function CountriesSearch(){
 
@@ -6,26 +7,34 @@ const[countries,setCountries]=useState([]);
 const[searchItem, setSearchItem]=useState('');
 
 useEffect(()=>{
-    fetch(' https://restcountries.com/v3.1/all').then(response=>response.json()).then(data=>setCountries(data)).catch(error=>console.error('Error fetching data',error))
+    fetch(' https://restcountries.com/v3.1/all').then(response=>response.json())
+    .then(data=>setCountries(data))
+    .catch(error=>console.error('Error fetching data',error))
+
 },[]);
-const filteredCountries=countries.filter(country=>country.name.common.toLowerCase().includes(searchItem.toLowerCase()))
+const filteredCountries=countries.filter(country=>country.name.common
+    .toLowerCase()
+    .includes(searchItem
+        .toLowerCase()))
 ;
+
 
 
     return(
         <div className='countryCard' >
-            <input
-            className='countryCard'
+            <div className='countryCard'><input
+            className='search-bar'
             type='text'
             placeholder='Search for a country..'
             value={searchItem}
-            onChange={e=>setSearchItem(e.target.value)} />
+            onChange={e=>setSearchItem(e.target.value)} /></div>
+            
 
-            <div >
-                {filteredCountries.length>0?(filteredCountries.map(country=>(<div key={country.cca3} className='countryCard' style={{display:'flex',flexDirection:'column',width:'100px',borderRadius:'8px',padding:'10px'}}>
-                        <img src={country.flags.png} alt={'Flag of ${country.name.common} '}  className='countryCard' style={{width:'100px',height:'auto',marginBottom:'10px'}}/>
-                        <p  className='countryCard'>{country.name.common}</p></div>
-                    ))):(<p>No matching countries found</p>)}
+            <div className='country-cards'>
+                {filteredCountries.length>0?(filteredCountries.map(country=>(<div key={country.cca3} className='countryCard' >
+                        <img src={country.flags.png} alt={'Flag of ${country.name.common} '}  />
+                        <p>{country.name.common}</p></div>
+                    ))):(<p></p>)}
             </div>
         </div>
     );
